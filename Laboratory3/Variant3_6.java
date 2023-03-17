98ca0e0987cac69bd000bc5116ed4a0374b6ba2d
unknown <dmitriykhotin@mail.ru>
2023-03-17 21:24:52 +0300

    Laboratory3: Variant3_6 done

--- Laboratory3/Variant3_6.java
+++ Laboratory3/Variant3_6.java
@@ -5,16 +5,175 @@
  * для каждого класса методы equals(), hashCode(), toString().
  * 6. Создать объект класса Роза, используя классы Лепесток, Бутон. Методы: расцвести, завять, вывести на консоль цвет бутона.
  * */
+
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        int multiply = 1;
-        int summary = 0;
-
-        for (int i = 0; i < args.length; i++) {
-            int number = Integer.parseInt(args[i]);
-            summary = summary + number;
-            multiply = multiply * number;
+        Petal[] petals = new Petal[5];
+        for (int i = 0; i < 5; i++) {
+            petals[i] = new Petal("red");
         }
-        System.out.println("Сумма " + summary + "\nПроизведение " + multiply);
+
+        Bud bud = new Bud("green", false, petals);
+        Rose rose = new Rose("Red rose", bud);
+        System.out.println(rose);
+
+        rose.bloom();
+        System.out.println(rose);
+
+        rose.wilt();
+        System.out.println(rose);
+    }
+}
+
+public class Rose {
+    private String name;
+    private Bud bud;
+
+    public Rose(String name, Bud bud) {
+        this.name = name;
+        this.bud = bud;
+    }
+
+    public void bloom() {
+        bud.setOpen(true);
+    }
+
+    public void wilt() {
+        bud.setOpen(false);
+    }
+
+    public String getBudColor() {
+        return bud.getColor();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Rose)) return false;
+        Rose rose = (Rose) o;
+        return name.equals(rose.name) && bud.equals(rose.bud);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + bud.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Rose{" +
+                "name='" + name + '\'' +
+                ", bud=" + bud +
+                '}';
+    }
+}
+
+public class Petal {
+    private String color;
+
+    public Petal(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Petal)) return false;
+        Petal petal = (Petal) o;
+        return color.equals(petal.color);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = color.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Petal{" +
+                "color='" + color +
+                '}';
+    }
+}
+
+public class Bud {
+    private String color;
+    private boolean isOpen;
+    private Petal[] petals;
+
+    public Bud(String color, boolean isOpen, Petal[] petals) {
+        this.color = color;
+        this.isOpen = isOpen;
+        this.petals = petals;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isOpen() {
+        return isOpen;
+    }
+
+    public void setOpen(boolean open) {
+        isOpen = open;
+    }
+
+    public void open() {
+        isOpen = true;
+    }
+
+    public void close() {
+        isOpen = false;
+    }
+
+    public Petal[] getPetals() {
+        return petals;
+    }
+
+    public void setPetals(Petal[] petals) {
+        this.petals = petals;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Bud)) return false;
+        Bud bud = (Bud) o;
+        return isOpen == bud.isOpen &&
+                Objects.equals(color, bud.color) &&
+                Arrays.equals(petals, bud.petals);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(color, isOpen);
+        result = 31 * result + Arrays.hashCode(petals);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Bud{" +
+                "color='" + color + '\'' +
+                ", isOpen=" + isOpen +
+                ", petals=" + Arrays.toString(petals) +
+                '}';
     }
 }
