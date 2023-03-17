a9d5af4693f75c12a0c197b78254f29785cffda7
unknown <dmitriykhotin@mail.ru>
2023-03-17 23:02:06 +0300

    Laboratory3: Variant4 done

--- Laboratory3/Variant4_7.java
+++ Laboratory3/Variant4_7.java
@@ -6,14 +6,121 @@
  * */
 public class Main {
     public static void main(String[] args) {
-        int multiply = 1;
-        int summary = 0;
+    }
+}
 
-        for (int i = 0; i < args.length; i++) {
-            int number = Integer.parseInt(args[i]);
-            summary = summary + number;
-            multiply = multiply * number;
-        }
-        System.out.println("Сумма " + summary + "\nПроизведение " + multiply);
+public class TelephoneExchange {
+    private List<Subscriber> subscribers; // список абонентов
+
+    public TelephoneExchange() {
+        this.subscribers = new ArrayList<>();
+    }
+
+    public void addSubscriber(Subscriber subscriber) {
+        subscribers.add(subscriber);
+    }
+
+    public void removeSubscriber(Subscriber subscriber) {
+        subscribers.remove(subscriber);
+    }
+
+    public List<Subscriber> getSubscribers() {
+        return subscribers;
+    }
+}
+
+public class Subscriber {
+    private String name;
+    private String phoneNumber;
+    private double account; // баланс абонента
+    private boolean isBlocked; // заблокирован ли абонент
+    private List<Service> services; // список подключенных услуг
+
+    public Subscriber(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.account = 0;
+        this.isBlocked = false;
+        this.services = new ArrayList<>();
+    }
+
+    public void addService(Service service) {
+        services.add(service);
+    }
+
+    public void removeService(Service service) {
+        services.remove(service);
+    }
+
+    public void changePhoneNumber(String newNumber) {
+        this.phoneNumber = newNumber;
+    }
+
+    public void payBill(double amount) {
+        account -= amount;
+    }
+
+    public void requestDisconnect() {
+        isBlocked = true;
+    }
+
+    public void requestReconnect() {
+        isBlocked = false;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public double getAccount() {
+        return account;
+    }
+
+    public boolean isBlocked() {
+        return isBlocked;
+    }
+
+    public List<Service> getServices() {
+        return services;
+    }
+}
+
+public class Service {
+    private String name;
+    private double cost;
+
+    public Service(String name, double cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+}
+
+public class Administrator {
+    public void changePhoneNumber(Subscriber subscriber, String newNumber) {
+        subscriber.changePhoneNumber(newNumber);
+    }
+
+    public void addService(Subscriber subscriber, Service service) {
+        subscriber.addService(service);
+    }
+
+    public void removeService(Subscriber subscriber, Service service) {
+        subscriber.removeService(service);
+    }
+
+    public void blockSubscriber(Subscriber subscriber) {
+        subscriber.requestDisconnect();
+    }
+
+    public void unblockSubscriber(Subscriber subscriber) {
+        subscriber.requestReconnect();
+    }
+
+    public void updateAccount(Subscriber subscriber, double amount) {
+        subscriber.payBill(amount);
     }
 }
--- Laboratory3/Variant4_8.java
+++ Laboratory3/Variant4_8.java
@@ -5,16 +5,216 @@
  * Водитель может сделать заявку на ремонт. Диспетчер может отстранить Водителя от работы.
  * Водитель делает отметку о выполнении Рейса и состоянии Автомобиля.
  * */
-public class Main {
-    public static void main(String[] args) {
-        int multiply = 1;
-        int summary = 0;
+import java.util.*;
 
-        for (int i = 0; i < args.length; i++) {
-            int number = Integer.parseInt(args[i]);
-            summary = summary + number;
-            multiply = multiply * number;
-        }
-        System.out.println("Сумма " + summary + "\nПроизведение " + multiply);
+
+public class Autobaza {
+    public static void main(String[] args) {
+        Dispatcher dispatcher = new Dispatcher();
+
+        // create some drivers
+        Driver driver1 = new Driver("John");
+        Driver driver2 = new Driver("Sarah");
+        Driver driver3 = new Driver("Bob");
+
+        // add drivers to dispatcher
+        dispatcher.addDriver(driver1);
+        dispatcher.addDriver(driver2);
+        dispatcher.addDriver(driver3);
+
+        // create some cars
+        Car car1 = new Car("Toyota", "Camry", 2010);
+        Car car2 = new Car("Honda", "Accord", 2012);
+        Car car3 = new Car("Nissan", "Altima", 2015);
+
+        // add cars to dispatcher
+        dispatcher.addCar(car1);
+        dispatcher.addCar(car2);
+        dispatcher.addCar(car3);
+
+        // assign driver1 to car1
+        dispatcher.assignDriverToCar(driver1, car1);
+
+        // mark driver1's trip as complete
+        dispatcher.markTripComplete(driver1);
+
+        // mark driver1's car for repair
+        dispatcher.markDriverForRepair(driver1);
+
+        // suspend driver1
+        dispatcher.suspendDriver(driver1);
+
+        // assign driver2 to car2
+        dispatcher.assignDriverToCar(driver2, car2);
+
+        // mark driver2's trip as complete
+        dispatcher.markTripComplete(driver2);
+
+        // suspend driver2
+        dispatcher.suspendDriver(driver2);
+
+        // reinstate driver2
+        dispatcher.reinstateDriver(driver2);
+
+        System.out.println(dispatcher.getDrivers());
+        System.out.println(dispatcher.getCars());
     }
 }
+
+class Driver {
+    private String name;
+    private boolean available;
+    private boolean onDuty;
+    private boolean needsRepair;
+    private Car car;
+
+    public Driver(String name) {
+        this.name = name;
+        this.available = true;
+        this.onDuty = false;
+        this.needsRepair = false;
+        this.car = null;
+    }
+
+    public void setOnDuty(boolean onDuty) {
+        this.onDuty = onDuty;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public void setNeedsRepair(boolean needsRepair) {
+        this.needsRepair = needsRepair;
+    }
+
+    public void assignCar(Car car) {
+        this.car = car;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public boolean isOnDuty() {
+        return onDuty;
+    }
+
+    public boolean needsRepair() {
+        return needsRepair;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public String toString() {
+        return "{ name: " + this.name + ", available: " + this.available + ", onDuty: " + this.onDuty + ", needsRepair: " + this.needsRepair + (this.car != null ? ", car: " + this.car.toString() : "") + "}";
+    }
+}
+
+class Car {
+    private String mark;
+    private String model;
+    private int year;
+    private boolean needsRepair;
+
+    public Car(String mark, String model, int year) {
+        this.mark = mark;
+        this.model = model;
+        this.year = year;
+        this.needsRepair = false;
+    }
+
+    public void setNeedsRepair(boolean needsRepair) {
+        this.needsRepair = needsRepair;
+    }
+
+    public boolean needsRepair() {
+        return needsRepair;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String toString() {
+        return "{ mark: " + this.mark + ", model: " + this.model + ", year: " + this.year + ", needsRepair: " + this.needsRepair;
+    }
+}
+
+class Dispatcher {
+    private ArrayList<Driver> drivers;
+    private ArrayList<Car> cars;
+    private String result;
+
+    public Dispatcher() {
+        this.drivers = new ArrayList<>();
+        this.cars = new ArrayList<>();
+    }
+
+    public void addDriver(Driver driver) {
+        drivers.add(driver);
+    }
+
+    public void removeDriver(Driver driver) {
+        drivers.remove(driver);
+    }
+
+    public void addCar(Car car) {
+        cars.add(car);
+    }
+
+    public void removeCar(Car car) {
+        cars.remove(car);
+    }
+
+    public void assignDriverToCar(Driver driver, Car car) {
+        if (driver.getCar() != null) {
+            driver.getCar().setNeedsRepair(true);
+        }
+        driver.assignCar(car);
+        driver.setAvailable(false);
+    }
+
+    public void markTripComplete(Driver driver) {
+        driver.setAvailable(true);
+        driver.getCar().setNeedsRepair(false);
+    }
+
+    public void markDriverForRepair(Driver driver) {
+        driver.setNeedsRepair(true);
+        driver.setAvailable(false);
+    }
+
+    public void suspendDriver(Driver driver) {
+        driver.setOnDuty(false);
+        driver.setAvailable(false);
+        driver.getCar().setNeedsRepair(false);
+    }
+
+    public void reinstateDriver(Driver driver) {
+        driver.setOnDuty(true);
+    }
+
+    public String getDrivers() {
+        result = "";
+        this.drivers.forEach(driver -> result += driver.toString() + "\n");
+        return result;
+    }
+
+    public String getCars() {
+        result = "";
+        this.cars.forEach(car -> result += car.toString() + "\n");
+        return result;
+    }
+}
+
