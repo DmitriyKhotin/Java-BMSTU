2292ad0267321fbf80c480829328702d23b33deb
unknown <dmitriykhotin@mail.ru>
2023-03-20 18:44:43 +0300

    Laboratory4: Variant2_7 done

--- Laboratory4/Variant2_7.java
+++ Laboratory4/Variant2_7.java
@@ -7,5 +7,46 @@ import java.util.*;
 public class Main
 {
     public static void main(String[] args) {
+        NeuroSurgeon neuroSurgeon = new NeuroSurgeon("Andrteey");
+        System.out.println(neuroSurgeon.getName());
+        neuroSurgeon.sayHello();
+    }
+}
+
+abstract class Doctor {
+    private String _name;
+
+    public Doctor(String name) {
+        this._name = name;
+    }
+
+    public String getName() {
+        return _name;
+    }
+
+    public void sayHello() {
+        System.out.println("Hello. I am a doctor!");
+    }
+}
+
+abstract class Surgeon extends Doctor {
+    public Surgeon(String name) {
+        super(name);
+    }
+
+    @Override
+    public void sayHello() {
+        System.out.println("Hello. I am a surgeon!");
+    }
+}
+
+class NeuroSurgeon extends Surgeon {
+    public NeuroSurgeon(String name) {
+        super(name);
+    }
+
+    @Override
+    public void sayHello() {
+        System.out.println("Hello. I am a neuro surgeon!");
     }
 }
